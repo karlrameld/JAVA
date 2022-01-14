828f6cb5b82c40ecac06420b5312c5afaede27d8
Kalle <karlrameld@gmail.com>
2022-01-14 16:50:49 +0100

    added +1

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++)
-            System.out.println(i);
+            System.out.println(i+1);
     }
 
 }
